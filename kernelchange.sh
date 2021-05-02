2907c9b876f80cec0f68bfafa2fe9e7d9f03bab9
welcomefrank <25167698+welcomefrank@users.noreply.github.com>
2021-05-02 21:56:31 +0800

    Update kernelchange.sh

--- kernelchange.sh
+++ kernelchange.sh
@@ -10,39 +10,60 @@ echo -e "本机目前内核如下:"
 uname -a
 cat /etc/redhat-release
 read -p "请确认本机Centos版本? 如7.2 需要保留1位小数" centosversion
+
+linkbroken(){
+if [ $? -eq 0 ];then
+echo "kernelfile downloaded successfully"
+else
+read -p "下载链接已坏 请重新提供新的下载链接" kernellinknew
+wget $kernellinknew
+kernelfile=${kernellinknew##*/}
+fi
+}
 if [ $centosversion = 6.7 ];then
-wget http://github.itzmx.com/1265578519/kernel/master/6.5/kernel-2.6.32-504.el6.x86_64.rpm
 kernelfile='kernel-2.6.32-504.el6.x86_64.rpm'
+wget http://github.itzmx.com/1265578519/kernel/master/6.5/kernel-2.6.32-504.el6.x86_64.rpm
+linkbroken
 elif [ $centosversion = 6.8 ];then
-wget http://ftp.scientificlinux.org/linux/scientific/6.6/x86_64/updates/security/kernel-2.6.32-504.3.3.el6.x86_64.rpm
 kernelfile='kernel-2.6.32-504.3.3.el6.x86_64.rpm'
+wget http://ftp.scientificlinux.org/linux/scientific/6.6/x86_64/updates/security/kernel-2.6.32-504.3.3.el6.x86_64.rpm
+linkbroken
 elif [ $centosversion = 6.9 ];then
-wget http://vault.centos.org/6.6/cr/x86_64/Packages/kernel-2.6.32-573.1.1.el6.x86_64.rpm
 kernelfile='kernel-2.6.32-573.1.1.el6.x86_64.rpm'
+wget http://vault.centos.org/6.6/cr/x86_64/Packages/kernel-2.6.32-573.1.1.el6.x86_64.rpm
+linkbroken
 elif [ $centosversion = 7.0 ];then
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
 kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+linkbroken
 elif [ $centosversion = 7.1 ];then
-wget http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm
 kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+linkbroken
 elif [ $centosversion = 7.2 ];then
+kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
 wget http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+linkbroken
 elif [ $centosversion = 7.3 ];then
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
 kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+linkbroken
 elif [ $centosversion = 7.4 ];then
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
 kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+linkbroken
 elif [ $centosversion = 7.5 ];then
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
 kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+linkbroken
 elif [ $centosversion = 7.6 ];then
+kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
 wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+linkbroken
 elif [ $centosversion = 7.7 ];then
-wget https://buildlogs.centos.org/c7.1511.00/kernel/20151119220809/3.10.0-327.el7.x86_64/kernel-3.10.0-327.el7.x86_64.rpm
 kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget https://buildlogs.centos.org/c7.1511.00/kernel/20151119220809/3.10.0-327.el7.x86_64/kernel-3.10.0-327.el7.x86_64.rpm
+linkbroken
 else
 read -p "暂时没有合适的 请根据本机内核填入合适的下载链接" kernellink
 wget $kernellink
@@ -55,6 +76,7 @@ else
 exit 0
 fi
 rpm -ivh $kernelfile --force
+rm -rf /root/$kernelfile
 echo -e "本机默认启动的内核 按顺序排列如下:"
 grub2-editenv list
 read -p "将重启机器使新安装内核生效 按任意键确认 按n回车表示放弃并退出" rebootvps 
