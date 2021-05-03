098b55eb5ced30665f694d92aa4007030fdb50a2
welcomefrank <25167698+welcomefrank@users.noreply.github.com>
2021-05-03 11:07:55 +0800

    Update kernelchange.sh

--- kernelchange.sh
+++ kernelchange.sh
@@ -1,3 +1,4 @@
+sudu su
 hostnamectl
 read -p "请确认本机架构是kvm/xen? 按n回车表示否并退出 按其它任意键继续" structurestatus 
 if [ $structurestatus = "n" ];then
@@ -21,54 +22,55 @@ kernelfile=${kernellinknew##*/}
 fi
 }
 if [ $centosversion = 6.7 ];then
-kernelfile='kernel-2.6.32-504.el6.x86_64.rpm'
-wget http://github.itzmx.com/1265578519/kernel/master/6.5/kernel-2.6.32-504.el6.x86_64.rpm
+kernellink='http://github.itzmx.com/1265578519/kernel/master/6.5/kernel-2.6.32-504.el6.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 6.8 ];then
-kernelfile='kernel-2.6.32-504.3.3.el6.x86_64.rpm'
-wget http://ftp.scientificlinux.org/linux/scientific/6.6/x86_64/updates/security/kernel-2.6.32-504.3.3.el6.x86_64.rpm
+kernellink='http://ftp.scientificlinux.org/linux/scientific/6.6/x86_64/updates/security/kernel-2.6.32-504.3.3.el6.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 6.9 ];then
-kernelfile='kernel-2.6.32-573.1.1.el6.x86_64.rpm'
-wget http://vault.centos.org/6.6/cr/x86_64/Packages/kernel-2.6.32-573.1.1.el6.x86_64.rpm
+kernellink='http://vault.centos.org/6.6/cr/x86_64/Packages/kernel-2.6.32-573.1.1.el6.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.0 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.1 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.2 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='http://ftp.scientificlinux.org/linux/scientific/7.1/x86_64/updates/security/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.3 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.4 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.5 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.6 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm
+kernellink='https://buildlogs.centos.org/c7.01.u/kernel/20150327030147/3.10.0-229.1.2.el7.x86_64/kernel-3.10.0-229.1.2.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 elif [ $centosversion = 7.7 ];then
-kernelfile='kernel-3.10.0-229.1.2.el7.x86_64.rpm'
-wget https://buildlogs.centos.org/c7.1511.00/kernel/20151119220809/3.10.0-327.el7.x86_64/kernel-3.10.0-327.el7.x86_64.rpm
+kernellink='https://buildlogs.centos.org/c7.1511.00/kernel/20151119220809/3.10.0-327.el7.x86_64/kernel-3.10.0-327.el7.x86_64.rpm'
+wget -P /root $kernellink
 linkbroken
 else
-read -p "暂时没有合适的 请根据本机内核填入合适的下载链接" kernellink
-wget $kernellink
-kernelfile=${kernellink##*/}
+read -p "暂时没有合适的 请根据本机内核填入合适的下载链接" kerneloklink
+wget -P /root $kerneloklink
+kernelfile=${kerneloklink##*/}
 fi
+kernelfile=${kernellink##*/}
 read -p "将在本机安装内核$kernelfile 按任意键确认 按n回车表示放弃并退出" confirmkernel 
 if [ $confirmkernel = "n" ];then
 exit 0 
