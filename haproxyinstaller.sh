2a2c9ce5200c00d1c020d380fe7ce3168301c88f
welcomefrank <25167698+welcomefrank@users.noreply.github.com>
2021-05-14 16:06:33 +0800

    Update haproxyinstaller.sh

--- haproxyinstaller.sh
+++ haproxyinstaller.sh
@@ -62,7 +62,7 @@ touch /root/haproxydata.txt
 echo -e "#The following rule for $rulefrontendport added on `date +20%y-%m-%d' '%H:%M:%S`" >> /root/haproxydata.txt  
 echo -e "$rulename $rulefrontendport $rulebackendip $rulebackendport" >> /root/haproxydata.txt
 cat >> /etc/haproxy/haproxy.cfg << EOF
-\n
+$\n
 frontend $rulename-in
         bind *:$rulefrontendport
         default_backend $rulename-out
