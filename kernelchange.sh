b079883f78f8d3eb437d488847d52bd98a9885a7
welcomefrank <25167698+welcomefrank@users.noreply.github.com>
2021-05-15 12:55:58 +0800

    Update kernelchange.sh

--- kernelchange.sh
+++ kernelchange.sh
@@ -1,4 +1,14 @@
+#fonts color
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+
 sudu su
+
+kernelchange(){
 hostnamectl
 read -p "请确认本机架构是kvm/xen? 按n回车表示否并退出 按其它任意键继续" structurestatus 
 if [ $structurestatus = "n" ];then
@@ -94,3 +104,61 @@ else
 echo -e "正在重启本机使新内核生效"
 reboot
 fi
+}
+
+Install_ServerSpeeder(){
+	[[ -e ${Server_Speeder_file} ]] && echo -e "${Error} 锐速(Server Speeder) 已安装 !" && exit 1
+	cd /root
+	wget -N --no-check-certificate https://raw.githubusercontent.com/welcomefrank/scripts/main/serverspeeder.sh
+	[[ ! -e "serverspeeder.sh" ]] && echo -e "${Error} 锐速安装脚本下载失败 !" && exit 1
+	bash serverspeeder.sh
+	sleep 2s
+	PID=`ps -ef |grep -v grep |grep "serverspeeder" |awk '{print $2}'`
+	if [[ ! -z ${PID} ]]; then
+		rm -rf /root/serverspeeder.sh
+		rm -rf /root/91yunserverspeeder
+		rm -rf /root/91yunserverspeeder.tar.gz
+		echo -e "${Info} 锐速(Server Speeder) 安装完成 !" && exit 1
+	else
+		echo -e "${Error} 锐速(Server Speeder) 安装失败 !" && exit 1
+	fi
+ln -s /serverspeeder/bin/serverSpeeder.sh /etc/init.d/serverspeeder
+if [ $? -eq 0 ]; then
+   echo -e "${GreenBG}已经将serverspeeder加入开机自动运行${Font}"
+   else
+   rm /etc/init.d/serverspeeder
+   ln -s /serverspeeder/bin/serverSpeeder.sh /etc/init.d/serverspeeder
+fi
+chkconfig --add /etc/init.d/serverspeeder
+service serverspeeder status
+menu
+}
+
+menu(){
+    echo -e "${Red}更换本机内核 + 锐速安装${Font}"
+    echo -e "${Green}1.${Font} 全自动更新本机内核"
+    echo -e "${Green}2.${Font} 安装锐速Serverspeeder并设置开机自动启动"
+    echo -e "${Green}3.${Font} 本机锐速运行状态"
+    echo -e "${Green}4.${Font}  退出 \n"
+    read -p "请输入数字：" menu_num
+    case $menu_num in
+        1)
+          kernelchange
+        ;;
+        2)
+          Install_ServerSpeeder
+        ;;
+        3)
+          service serverspeeder status
+          menu
+        ;;
+        4)
+          exit 0
+          ;;
+        *)
+          echo -e "${RedBG}请输入正确的数字${Font}"
+          ;;
+    esac
+}
+
+menu
